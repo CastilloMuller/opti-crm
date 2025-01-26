80d76b23fb3c6288ece4890375733a229761644c
CastilloMuller <your.email@example.com>
2025-01-26 14:34:48 +0100

    Update vite config for proper module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
+    dedupe: ["vue"],
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": "/src",
     },
   },
-})
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
+});
